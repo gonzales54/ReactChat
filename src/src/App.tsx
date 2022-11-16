c1d6d76cba8cc3b5fe68032f966c8fe9a65df2a2
gonzales54 <gonzalesitokawa@gmail.com>
2022-11-16 21:41:14 +0900

    start project

---
+++ src/src/App.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { db } from './FirebaseConfig';
+import { collection, DocumentData, getDocs, QuerySnapshot } from 'firebase/firestore';
+
+function App() {
+  const [allUser, setAllUser] = useState<any>([])
+
+  const docRef = collection(db, "users")
+
+  useEffect(() => {
+    getDocs(docRef)
+      .then((snapshot: QuerySnapshot<DocumentData>) => {
+        console.log(snapshot.docs.map((doc) => ({ ...doc.data() })))
+        setAllUser(snapshot.docs.map((doc) => ({ ...doc.data() })));
+      })
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      {allUser.map((item: any, index: number) => {
+        return (
+          <p key={index}>{item.information.name}</p>
+        )
+      })}
+    </div>
+  )
+}
+
+export default App
